e07dbef6e5abccfe57de76eec1605965d9e9e9e2
Denis Costello <G00396309@atu.ie>
2022-10-19 20:28:56 +0100

    Added a new test increament

--- src/main/java/ie/atu/Exercise1/Counter.java
+++ src/main/java/ie/atu/Exercise1/Counter.java
@@ -1,4 +1,14 @@
 package ie.atu.Exercise1;
 
 public class Counter {
+    private int countValue;
+
+    public Counter(){
+        countValue = 0;
+    }
+
+    public int increament(){
+        countValue++;
+        return countValue;
+    }
 }
--- src/test/java/ie/atu/Exercise1/CounterTest.java
+++ src/test/java/ie/atu/Exercise1/CounterTest.java
@@ -2,13 +2,21 @@ package ie.atu.Exercise1;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CounterTest {
+    Counter myCount;
 
     @BeforeEach
     void setUp() {
+        myCount = new Counter();
+    }
+
+    @Test
+    void testincreament(){
+        assertEquals(1,myCount.increament());
     }
 
     @AfterEach
